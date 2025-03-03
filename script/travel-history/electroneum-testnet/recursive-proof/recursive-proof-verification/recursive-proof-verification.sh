a191d991a0eec7cea668d3050423a8007a5bab5e
masaun <masaun@protonmail.com>
2025-03-03 21:55:05 +0900

    [Travel History - script]: Implement the Recursive Proof generation/verification script

---
+++ script/travel-history/electroneum-testnet/recursive-proof/recursive-proof-verification/recursive-proof-verification.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: ./recursive-proof-generation.sh"
+  exit 1
+fi
+
+echo "Run the ./recursive-proof-generation.ts"
+node ./recursiveProofVerification.ts
+
+# cp ./circuits/Nargo.toml /tmp/$1/Nargo.toml
+# cp ./circuits/Verifier.toml /tmp/$1/Verifier.toml
+# cp -r ./circuits/src /tmp/$1/
+# echo "" > /tmp/$1/Prover.toml && echo "File created"
