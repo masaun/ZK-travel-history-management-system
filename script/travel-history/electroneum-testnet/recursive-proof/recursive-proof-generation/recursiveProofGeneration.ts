06823d12848ba208f0ac031a43d9ab86da40facf
masaun <masaun@protonmail.com>
2025-03-04 09:41:09 +0900

    [Travel History - script]: Separate by 3 functions (= getCircuit(), setUp(), generateProof()) in the recursiveProofGeneration.ts

--- script/travel-history/electroneum-testnet/recursive-proof/recursive-proof-generation/recursiveProofGeneration.ts
+++ script/travel-history/electroneum-testnet/recursive-proof/recursive-proof-generation/recursiveProofGeneration.ts
@@ -15,76 +15,76 @@ let circuits: Circuits;
 let backends: BackendInstances;
 let noirs: Noirs;
 
-circuits = {
-    main: await getCircuit('main'),
-    recursive: await getCircuit('recursion'),
-};
-
-backends = {
-    main: new BarretenbergBackend(circuits.main, { threads: 8 }),
-    recursive: new BarretenbergBackend(circuits.recursive, { threads: 8 }),
-};
-
-noirs = {
-    main: new Noir(circuits.main, backends.main),
-    recursive: new Noir(circuits.recursive, backends.recursive),
-};
-
-  after(async () => {
-    await backends.main.destroy();
-    await backends.recursive.destroy();
-  });
-
-  describe('Recursive flow', async () => {
-    let recursiveInputs: any;
-    let intermediateProof: ProofData;
-    let finalProof: ProofData;
-
-    describe.only('Proof generation', async () => {
-      it('Should generate an intermediate proof', async () => {
-        const { witness } = await noirs.main.execute(mainInput);
-        intermediateProof = await backends.main.generateProof(witness);
-
-        const { proof, publicInputs } = intermediateProof;
-        expect(proof instanceof Uint8Array).to.be.true;
-
-        const verified = await backends.main.verifyProof({ proof, publicInputs });
-        expect(verified).to.be.true;
-
-        const numPublicInputs = 1;
-        const { proofAsFields, vkAsFields, vkHash } =
-          await backends.main.generateRecursiveProofArtifacts(
-            { publicInputs, proof },
-            numPublicInputs,
-          );
-        expect(vkAsFields).to.be.of.length(114); /// @dev - Must be [Field, 114]
-        expect(vkHash).to.be.a('string');
-
-        recursiveInputs = {
-          verification_key: vkAsFields,
-          proof: proofAsFields,
-          public_inputs: [mainInput.y],
-          key_hash: vkHash,
-        };
-      });
-
-      it('Should generate a final proof with a recursive input', async () => {
-        finalProof = await noirs.recursive.generateProof(recursiveInputs);
-        expect(finalProof.proof instanceof Uint8Array).to.be.true;
-      });
-    });
-
-
 
 /**
  * @notice - Get the compiled circuit
  */
 async function getCircuit(name: string) {
-    const basePath = resolve(join('../noir', name));
-    const fm = createFileManager(basePath);
-    const compiled = await compile(fm, basePath);
-    if (!('program' in compiled)) {
-      throw new Error('Compilation failed');
-    }
-    return compiled.program;
+  const basePath = resolve(join('../noir', name));
+  const fm = createFileManager(basePath);
+  const compiled = await compile(fm, basePath);
+  if (!('program' in compiled)) {
+    throw new Error('Compilation failed');
+  }
+  return compiled.program;
+}
+
+
+/**
+ * @notice - Setup configuration
+ */
+async function setUp() {
+    circuits = {
+        main: await getCircuit('main'),
+        recursive: await getCircuit('recursion'),
+    };
+
+    backends = {
+        main: new BarretenbergBackend(circuits.main, { threads: 8 }),
+        recursive: new BarretenbergBackend(circuits.recursive, { threads: 8 }),
+    };
+
+    noirs = {
+        main: new Noir(circuits.main, backends.main),
+        recursive: new Noir(circuits.recursive, backends.recursive),
+    };
+}
+
+
+/**
+ * @notice - Proof generation
+ */
+async function generateProof() {
+  let recursiveInputs: any;
+  let intermediateProof: ProofData;
+  let finalProof: ProofData;
+
+  const { witness } = await noirs.main.execute(mainInput);
+    intermediateProof = await backends.main.generateProof(witness);
+
+    const { proof, publicInputs } = intermediateProof;
+    expect(proof instanceof Uint8Array).to.be.true;
+
+    const verified = await backends.main.verifyProof({ proof, publicInputs });
+    expect(verified).to.be.true;
+
+    const numPublicInputs = 1;
+    const { proofAsFields, vkAsFields, vkHash } =
+        await backends.main.generateRecursiveProofArtifacts(
+            { publicInputs, proof },
+            numPublicInputs,
+        );
+    expect(vkAsFields).to.be.of.length(114); /// @dev - Must be [Field, 114]
+    expect(vkHash).to.be.a('string');
+
+    recursiveInputs = {
+        verification_key: vkAsFields,
+        proof: proofAsFields,
+        public_inputs: [mainInput.y],
+        key_hash: vkHash,
+    };
+
+    /// @dev - Generate a final proof with a recursive input
+    finalProof = await noirs.recursive.generateProof(recursiveInputs);
+    expect(finalProof.proof instanceof Uint8Array).to.be.true;
 }
