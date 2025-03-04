64894d85f954466064d69a40c35729660420c235
masaun <masaun@protonmail.com>
2025-03-04 10:59:26 +0900

    [Travel History - script]: Add the README

---
+++ script/travel-history/electroneum-testnet/recursive-proof/README.md
@@ -0,0 +1,8 @@
+# Recursion Proof generation/verification script
+
+## Installation
+
+- 1/ Install NPM modules
+```bash
+npm i --force
+```
--- script/travel-history/electroneum-testnet/recursive-proof/recursive-proof-generation/recursive-proof-generation.sh
+++ script/travel-history/electroneum-testnet/recursive-proof/recursive-proof-generation/recursive-proof-generation.sh
@@ -7,8 +7,3 @@ fi
 
 echo "Run the ./recursive-proof-generation.ts"
 node ./recursiveProofGeneration.ts
-
-# cp ./circuits/Nargo.toml /tmp/$1/Nargo.toml
-# cp ./circuits/Verifier.toml /tmp/$1/Verifier.toml
-# cp -r ./circuits/src /tmp/$1/
-# echo "" > /tmp/$1/Prover.toml && echo "File created"
