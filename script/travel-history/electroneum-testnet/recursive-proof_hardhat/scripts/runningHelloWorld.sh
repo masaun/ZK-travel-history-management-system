02832a491c93a5a7611d13fce34af19482993cbd
masaun <masaun@protonmail.com>
2025-03-06 08:54:28 +0900

    [All - circuit & SCs & test]: Modify the directory composition -> Put circuits/SCs/tests together into a single one

--- script/travel-history/electroneum-testnet/recursive-proof_hardhat/scripts/runningHelloWorld.sh
+++
@@ -1,3 +0,0 @@
-echo "Run the ./scripts/helloWorld.ts"
-npx hardhat run scripts/helloWorld.ts
-#npx hardhat run scripts/helloWorld.ts
--- script/travel-history/electroneum-testnet/verify-script.sh
+++
@@ -1,5 +0,0 @@
-echo "Load the environment variables from the .env file..."
-. ./.env
-
-echo "Verifying a proof via the Starter (UltraVerifier) contract on Sonic Blaze Testnet..."
-forge script script/electroneum-testnet/Verify.s.sol --broadcast --private-key ${SONIC_BLAZE_TESTNET_PRIVATE_KEY} --skip-simulation
