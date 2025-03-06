6093936079b7226a6403f1c914ba612295998c10
masaun <masaun@protonmail.com>
2025-03-06 14:55:26 +0900

    [circuit]: Rename with the 'travel_history' in the build.sh

--- circuits/build.sh
+++ circuits/build.sh
@@ -8,19 +8,19 @@ echo "Generate witness..."
 nargo execute
 
 echo "Proving and generating a ZK Proof..."
-bb prove -b ./target/traveled_country.json -w ./target/traveled_country.gz -o ./target/traveled_country_proof.bin
+bb prove -b ./target/travel_history.json -w ./target/travel_history.gz -o ./target/travel_history_proof.bin
 
 echo "Generating vkey..."
-bb write_vk -b ./target/traveled_country.json -o ./target/traveled_country_vk.bin
+bb write_vk -b ./target/travel_history.json -o ./target/travel_history_vk.bin
 
 echo "Link vkey to the zkProof"
-bb verify -k ./target/traveled_country_vk.bin -p ./target/traveled_country_proof.bin
+bb verify -k ./target/travel_history_vk.bin -p ./target/travel_history_proof.bin
 
 echo "Check a zkProof"
-head -c 32 ./target/traveled_country_proof.bin | od -An -v -t x1 | tr -d $' \n'
+head -c 32 ./target/travel_history_proof.bin | od -An -v -t x1 | tr -d $' \n'
 
 echo "Copy and paste vk for generating a Solidity Verifier contract"
-cp ./target/traveled_country_vk.bin ./target/vk
+cp ./target/travel_history_vk.bin ./target/vk
 
 echo "Generate a Solidity Verifier contract"
 bb contract
