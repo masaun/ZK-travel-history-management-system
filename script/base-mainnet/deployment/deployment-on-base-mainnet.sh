8b31c879bef174973928e62fa80809630c8a72fc
masaun <masaun@protonmail.com>
2025-07-19 15:08:09 +0900

    [SC script - deployment]: Fix the path in the sh file

--- script/base-mainnet/deployment/deployment-on-base-mainnet.sh
+++ script/base-mainnet/deployment/deployment-on-base-mainnet.sh
@@ -2,7 +2,7 @@ echo "Load the environment variables from the .env file..."
 . ./.env
 
 echo "Deploying the UltraVerifier and TravelHistoryProofVerifier contract on Base Sepolia Testnet..."
-forge script script/base-testnet/deployment/DeploymentAllContracts.s.sol \
+forge script script/base-mainnet/deployment/DeploymentAllContracts.s.sol \
     --broadcast \
     --rpc-url ${BASE_MAINNET_RPC} \
     --chain-id ${BASE_MAINNET_CHAIN_ID} \
@@ -14,7 +14,7 @@ forge script script/base-testnet/deployment/DeploymentAllContracts.s.sol \
 # [NOTE - Adding the "--legacy" option]: Due to this error - Error: Failed to estimate EIP1559 fees. This chain might not support EIP1559, try adding --legacy to your command.
 
 echo "Verify the deployed-smart contracts on Base Sepolia Testnet Explorer..."
-forge script script/base-testnet/deployment/DeploymentAllContracts.s.sol \
+forge script script/base-mainnet/deployment/DeploymentAllContracts.s.sol \
     --rpc-url ${BASE_MAINNET_RPC} \
     --chain-id ${BASE_MAINNET_CHAIN_ID} \
     --private-key ${BASE_MAINNET_PRIVATE_KEY} \
