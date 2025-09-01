58f9cc31a03650371dd0e1b99c5e13ce1125c83f
masaun <masaun@protonmail.com>
2025-09-01 11:54:29 +0900

    [SC]: Define the getRewards() in the StakingPool.sol

--- contracts/StakingPool.sol
+++ contracts/StakingPool.sol
@@ -5,14 +5,14 @@ pragma solidity ^0.8.25;
  */
 contract StakingPool {
     mapping(address => mapping(uint256 => string)) public checkpoints;
-    mapping(address caller => mapping(uint256 blockTimestamp => uint256 count)) public checkpointCounts;
+    mapping(address caller => uint256 count) public checkpointCounts;
     mapping(address => bool) public stakers;
     mapping(address => uint256) public stakedAmounts;
 
     string public version;
 
     constructor() {
-        version = "0.2.28";
+        version = "0.2.29";
     }
 
     /**
@@ -86,18 +86,27 @@ contract StakingPool {
     }
 
     /**
+     * @notice - Get the rewards based on the count of a caller's checkpoints
+     */
+    function getRewards() public view returns (bool) {
+        uint256 rewardAmount = checkpointCounts[msg.sender] * 1 ether;  // 1 ether reward per checkpoint
+        require(rewardAmount > 0, "No rewards available");
+        return true;
+    }
+
+    /**
      * @notice - checkpoint function
      */
     function checkpoint(string memory methodName) public returns (bool) {
         checkpoints[msg.sender][block.timestamp] = methodName;
         checkpoints[msg.sender][block.timestamp] = "checkpoint";
-        checkpointCounts[msg.sender][block.timestamp]++;
+        checkpointCounts[msg.sender]++;
         return true;
     }
 
     function testFunctionForCheckPoint() public returns (bool) {
         checkpoints[msg.sender][block.timestamp] = "testFunctionForCheckPoint";
-        checkpointCounts[msg.sender][block.timestamp]++;
+        checkpointCounts[msg.sender]++;
         return true;
     }
 
--- contracts/TravelHistoryManager.sol
+++ contracts/TravelHistoryManager.sol
@@ -24,7 +24,7 @@ contract TravelHistoryManager {
 
     constructor(TravelHistoryProofVerifier _travelHistoryProofVerifier) {
         travelHistoryProofVerifier = _travelHistoryProofVerifier;
-        version = "0.2.28";
+        version = "0.2.29";
     }
 
     /**
