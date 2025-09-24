2f7529d1370e0692d30b1fc5a11b7129369524ab
masaun <masaun@protonmail.com>
2025-09-24 18:04:04 +0900

    [SC]: Add the Balancer v3 related repos to the /defi-protocols

---
+++ contracts/defi-protocols/balancer-v3/pkg/vault/contracts/CompositeLiquidityRouter.sol
@@ -0,0 +1,200 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import { IPermit2 } from "permit2/src/interfaces/IPermit2.sol";
+
+import { ICompositeLiquidityRouter } from "@balancer-labs/v3-interfaces/contracts/vault/ICompositeLiquidityRouter.sol";
+import { IWETH } from "@balancer-labs/v3-interfaces/contracts/solidity-utils/misc/IWETH.sol";
+import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
+import "@balancer-labs/v3-interfaces/contracts/vault/RouterTypes.sol";
+
+import { CompositeLiquidityRouterHooks } from "./CompositeLiquidityRouterHooks.sol";
+import { CompositeLiquidityRouterQueries } from "./CompositeLiquidityRouterQueries.sol";
+
+/**
+ * @notice Entrypoint for add/remove liquidity operations on ERC4626 and nested pools.
+ * @dev The external API functions unlock the Vault, which calls back into the corresponding hook functions.
+ * These execute the steps needed to add to and remove liquidity from these special types of pools, and settle
+ * the operation with the Vault.
+ */
+contract CompositeLiquidityRouter is ICompositeLiquidityRouter, CompositeLiquidityRouterQueries {
+    constructor(
+        IVault vault,
+        IWETH weth,
+        IPermit2 permit2,
+        string memory routerVersion
+    ) CompositeLiquidityRouterQueries(vault, weth, permit2, routerVersion) {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
+    /*******************************************************************************
+                                ERC4626 Pools
+    *******************************************************************************/
+
+    /// @inheritdoc ICompositeLiquidityRouter
+    function addLiquidityUnbalancedToERC4626Pool(
+        address pool,
+        bool[] memory wrapUnderlying,
+        uint256[] memory exactAmountsIn,
+        uint256 minBptAmountOut,
+        bool wethIsEth,
+        bytes memory userData
+    ) external payable saveSender(msg.sender) returns (uint256 bptAmountOut) {
+        bptAmountOut = abi.decode(
+            _vault.unlock(
+                abi.encodeCall(
+                    CompositeLiquidityRouterHooks.addLiquidityERC4626PoolUnbalancedHook,
+                    (
+                        AddLiquidityHookParams({
+                            sender: msg.sender,
+                            pool: pool,
+                            maxAmountsIn: exactAmountsIn,
+                            minBptAmountOut: minBptAmountOut,
+                            kind: AddLiquidityKind.UNBALANCED,
+                            wethIsEth: wethIsEth,
+                            userData: userData
+                        }),
+                        wrapUnderlying
+                    )
+                )
+            ),
+            (uint256)
+        );
+    }
+
+    /// @inheritdoc ICompositeLiquidityRouter
+    function addLiquidityProportionalToERC4626Pool(
+        address pool,
+        bool[] memory wrapUnderlying,
+        uint256[] memory maxAmountsIn,
+        uint256 exactBptAmountOut,
+        bool wethIsEth,
+        bytes memory userData
+    ) external payable saveSender(msg.sender) returns (uint256[] memory) {
+        return
+            abi.decode(
+                _vault.unlock(
+                    abi.encodeCall(
+                        CompositeLiquidityRouterHooks.addLiquidityERC4626PoolProportionalHook,
+                        (
+                            AddLiquidityHookParams({
+                                sender: msg.sender,
+                                pool: pool,
+                                maxAmountsIn: maxAmountsIn,
+                                minBptAmountOut: exactBptAmountOut,
+                                kind: AddLiquidityKind.PROPORTIONAL,
+                                wethIsEth: wethIsEth,
+                                userData: userData
+                            }),
+                            wrapUnderlying
+                        )
+                    )
+                ),
+                (uint256[])
+            );
+    }
+
+    /// @inheritdoc ICompositeLiquidityRouter
+    function removeLiquidityProportionalFromERC4626Pool(
+        address pool,
+        bool[] memory unwrapWrapped,
+        uint256 exactBptAmountIn,
+        uint256[] memory minAmountsOut,
+        bool wethIsEth,
+        bytes memory userData
+    ) external payable saveSender(msg.sender) returns (uint256[] memory) {
+        return
+            abi.decode(
+                _vault.unlock(
+                    abi.encodeCall(
+                        CompositeLiquidityRouterHooks.removeLiquidityERC4626PoolProportionalHook,
+                        (
+                            RemoveLiquidityHookParams({
+                                sender: msg.sender,
+                                pool: pool,
+                                minAmountsOut: minAmountsOut,
+                                maxBptAmountIn: exactBptAmountIn,
+                                kind: RemoveLiquidityKind.PROPORTIONAL,
+                                wethIsEth: wethIsEth,
+                                userData: userData
+                            }),
+                            unwrapWrapped
+                        )
+                    )
+                ),
+                (uint256[])
+            );
+    }
+
+    /***************************************************************************
+                                   Nested pools
+    ***************************************************************************/
+
+    /// @inheritdoc ICompositeLiquidityRouter
+    function addLiquidityUnbalancedNestedPool(
+        address parentPool,
+        address[] memory tokensIn,
+        uint256[] memory exactAmountsIn,
+        address[] memory tokensToWrap,
+        uint256 minBptAmountOut,
+        bool wethIsEth,
+        bytes memory userData
+    ) external payable saveSender(msg.sender) returns (uint256) {
+        return
+            abi.decode(
+                _vault.unlock(
+                    abi.encodeCall(
+                        CompositeLiquidityRouterHooks.addLiquidityUnbalancedNestedPoolHook,
+                        (
+                            AddLiquidityHookParams({
+                                pool: parentPool,
+                                sender: msg.sender,
+                                maxAmountsIn: exactAmountsIn,
+                                minBptAmountOut: minBptAmountOut,
+                                kind: AddLiquidityKind.UNBALANCED,
+                                wethIsEth: wethIsEth,
+                                userData: userData
+                            }),
+                            tokensIn,
+                            tokensToWrap
+                        )
+                    )
+                ),
+                (uint256)
+            );
+    }
+
+    /// @inheritdoc ICompositeLiquidityRouter
+    function removeLiquidityProportionalNestedPool(
+        address parentPool,
+        uint256 exactBptAmountIn,
+        address[] memory tokensOut,
+        uint256[] memory minAmountsOut,
+        address[] memory tokensToUnwrap,
+        bool wethIsEth,
+        bytes memory userData
+    ) external payable saveSender(msg.sender) returns (uint256[] memory amountsOut) {
+        amountsOut = abi.decode(
+            _vault.unlock(
+                abi.encodeCall(
+                    CompositeLiquidityRouterHooks.removeLiquidityProportionalNestedPoolHook,
+                    (
+                        RemoveLiquidityHookParams({
+                            sender: msg.sender,
+                            pool: parentPool,
+                            minAmountsOut: minAmountsOut,
+                            maxBptAmountIn: exactBptAmountIn,
+                            kind: RemoveLiquidityKind.PROPORTIONAL,
+                            wethIsEth: wethIsEth,
+                            userData: userData
+                        }),
+                        tokensOut,
+                        tokensToUnwrap
+                    )
+                )
+            ),
+            (uint256[])
+        );
+    }
+}
